6518f54e539e905fbab16d4549653e2e2829d766
Jhonatan Wendel <wendeljhonatan@gmail.com>
2022-11-22 13:12:16 -0300

    Consertando a questão 1 da lista 6 para o que o professor requisitou

--- fundprog/resolucao_de_listas/lista6/l6q1.py
+++ fundprog/resolucao_de_listas/lista6/l6q1.py
@@ -5,7 +5,17 @@ for i in range(5):
     nome = input(f'Digite o nome do cliente: ')
     saldo = float(input('Digite o saldo na conta do cliente: '))
     op = input('Informe a operação que o cliente fará ["C" ou "D"]: '.upper())
-    cliente = [numconta, nome, saldo, op]
+    saldo_atualizado = None
+    match op:
+        case 'C':
+            credito = float(input('Quanto você quer creditar?\n->'))
+            saldo_atualizado = saldo + credito
+        case 'D':
+            debito = float(input('Quanto você quer debitar?\n->'))
+            saldo_atualizado = saldo - debito
+        case _:
+            print('Operação não reconhecida, nenhum alteração será executada.')
+    cliente = [numconta, nome, saldo, op, saldo_atualizado]
 
     contas.append(cliente)
 
