37b15aa7de251ac71f2956f7e5b5b270a9d990bf
jhonatanwen <wendeljhonatan@gmail.com>
2023-02-28 22:42:00 -0300

    refatorando l1q22

--- labprog/lista1/l1q22.c
+++ labprog/lista1/l1q22.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
 int main() {
-	printf("Inteiro equivalente a A: %d\n", 'A');
-	printf("Inteiro equivalente a B: %d\n", 'B');
-	printf("Inteiro equivalente a C: %d\n", 'C');
-	printf("Inteiro equivalente a a: %d\n", 'a');
-	printf("Inteiro equivalente a b: %d\n", 'b');
-	printf("Inteiro equivalente a c: %d\n", 'c');
-	printf("Inteiro equivalente a 0: %d\n", '0');
-	printf("Inteiro equivalente a 1: %d\n", '1');
-	printf("Inteiro equivalente a 2: %d\n", '2');
-	printf("Inteiro equivalente ao caractere $: %d\n", '$');
-	printf("Inteiro equivalente ao caractere *: %d\n", '*');
-	printf("Inteiro equivalente ao caractere +: %d\n", '+');
-	printf("Inteiro equivalente ao caractere /: %d\n", '/');
-	printf("Inteiro equivalente ao caractere espaço em branco: %d\n", ' ');
+	char c = ' ';
+
+	printf("Entre um caractere: ");
+	scanf("%c", &c);
+
+	printf("Inteiro equivalente a %c: %d\n", c, c);
+	// Inteiro equivalente a A: 65
+	// Inteiro equivalente a B: 66
+	// Inteiro equivalente a C: 67
+	// Inteiro equivalente a a: 97
+	// Inteiro equivalente a b: 98
+	// Inteiro equivalente a c: 99
+	// Inteiro equivalente a 0: 48
+	// Inteiro equivalente a 1: 49
+	// Inteiro equivalente a 2: 50
+	// Inteiro equivalente a $: 36
+	// Inteiro equivalente a *: 42
+	// Inteiro equivalente a +: 43
+	// Inteiro equivalente a /: 47
+	// Inteiro equivalente a  : 32
 
 	return 0;
 }
